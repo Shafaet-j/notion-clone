5cb225548ac1ef7b1844999d6ffc2a997a9c9718
Shafaet-j <lbshaft01@gmail.com>
2023-12-02 13:04:33 +0600

    Notion setup done

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Heading from "./_components/heading";
+
+export default function Marketing() {
+  return (
+    <div className=" min-h-full flex flex-col">
+      <div className=" flex flex-col items-center justify-center md:justify-start text-center gap-y-7 flex-1 pb-8">
+        <Heading />
+      </div>
+    </div>
+  );
+}
